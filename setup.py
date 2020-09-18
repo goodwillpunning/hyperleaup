c56174e11bfe79c58c1f94935f224f73bf0c7a35
Will Girten <will.girten@databricks.com>
2020-09-18 10:11:37 -0400

    Add support for short data type.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hyperleaup',
-    version='0.0.4',
+    version='0.0.5',
     packages=['hyperleaup'],
     url='https://github.com/goodwillpunning/hyperleaup',
     license='None',
